d5be17e8e9e0a37a124784ef78f0867d44af93f2
ozark-dev <jason@ozarkdev.com>
2023-05-14 20:56:33 -0500

    Developed placeOrder method to build our order and return orderTrackingNumber

--- src/main/java/com/kaminocode/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/kaminocode/ecommerce/service/CheckoutServiceImpl.java
@@ -3,8 +3,14 @@ package com.kaminocode.ecommerce.service;
 import com.kaminocode.ecommerce.dao.CustomerRepository;
 import com.kaminocode.ecommerce.dto.Purchase;
 import com.kaminocode.ecommerce.dto.PurchaseResponse;
+import com.kaminocode.ecommerce.entity.Customer;
+import com.kaminocode.ecommerce.entity.Order;
+import com.kaminocode.ecommerce.entity.OrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -18,6 +24,37 @@ public class CheckoutServiceImpl implements CheckoutService{
 
     @Override
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        // save to the database
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number (UUID version-4)
+        return UUID.randomUUID().toString();
+
     }
 }
