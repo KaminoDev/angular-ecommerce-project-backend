8f1becfc20c8c352b1de350ff38308fb517f6c35
ozark-dev <jason@ozarkdev.com>
2023-05-14 21:02:40 -0500

    Injected the checkoutService into the CheckoutController

--- src/main/java/com/kaminocode/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/kaminocode/ecommerce/controller/CheckoutController.java
@@ -1,4 +1,22 @@
 package com.kaminocode.ecommerce.controller;
 
+import com.kaminocode.ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/checkout")
 public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+
 }
