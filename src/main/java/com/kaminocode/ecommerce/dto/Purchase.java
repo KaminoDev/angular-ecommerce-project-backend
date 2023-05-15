bccaddeece80753eda515c400c66edc7e05f9518
ozark-dev <jason@ozarkdev.com>
2023-05-14 20:17:30 -0500

    Added the relationships to customer, shippingAddress, billingAddress, order, and orderItems

--- src/main/java/com/kaminocode/ecommerce/dto/Purchase.java
+++ src/main/java/com/kaminocode/ecommerce/dto/Purchase.java
@@ -1,4 +1,19 @@
 package com.kaminocode.ecommerce.dto;
 
+import com.kaminocode.ecommerce.entity.Address;
+import com.kaminocode.ecommerce.entity.Customer;
+import com.kaminocode.ecommerce.entity.Order;
+import com.kaminocode.ecommerce.entity.OrderItem;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class Purchase {
+
+    private Customer customer;
+    private Address shippingAddress;
+    private Address billingAddress;
+    private Order order;
+    private Set<OrderItem> orderItems;
 }
