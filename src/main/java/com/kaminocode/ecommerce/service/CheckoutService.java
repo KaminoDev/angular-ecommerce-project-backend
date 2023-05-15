2fd6868fc4953c794642e95c3d41bfa36b1eb4f3
ozark-dev <jason@ozarkdev.com>
2023-05-14 20:27:59 -0500

    Added PurchaseResponse object that takes in a purchase dto

--- src/main/java/com/kaminocode/ecommerce/service/CheckoutService.java
+++ src/main/java/com/kaminocode/ecommerce/service/CheckoutService.java
@@ -1,4 +1,9 @@
 package com.kaminocode.ecommerce.service;
 
+import com.kaminocode.ecommerce.dto.Purchase;
+import com.kaminocode.ecommerce.dto.PurchaseResponse;
+
 public interface CheckoutService {
+
+    PurchaseResponse placeOrder(Purchase purchase);
 }
