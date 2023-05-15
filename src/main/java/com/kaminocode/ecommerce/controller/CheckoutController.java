553aadea770e3a561fc506608fbaceb5a2c9e7c9
ozark-dev <jason@ozarkdev.com>
2023-05-14 21:06:38 -0500

    Developed purchaseOrder method that returns a purchaseRespone

--- src/main/java/com/kaminocode/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/kaminocode/ecommerce/controller/CheckoutController.java
@@ -1,10 +1,10 @@
 package com.kaminocode.ecommerce.controller;
 
+import com.kaminocode.ecommerce.dto.Purchase;
+import com.kaminocode.ecommerce.dto.PurchaseResponse;
 import com.kaminocode.ecommerce.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -16,6 +16,14 @@ public class CheckoutController {
     @Autowired
     public CheckoutController(CheckoutService checkoutService) {
         this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
     }
 
 
