b13816515778292ba8d9a8de9b37a0e9a04297ce
ozark-dev <jason@ozarkdev.com>
2023-05-14 20:34:03 -0500

    Setup CheckoutServiceImpl; injected CustomerRepository

--- src/main/java/com/kaminocode/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/kaminocode/ecommerce/service/CheckoutServiceImpl.java
@@ -1,4 +1,23 @@
 package com.kaminocode.ecommerce.service;
 
-public class CheckoutServiceImpl {
+import com.kaminocode.ecommerce.dao.CustomerRepository;
+import com.kaminocode.ecommerce.dto.Purchase;
+import com.kaminocode.ecommerce.dto.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        return null;
+    }
 }
